dc8cc587f8a7bba0029f343326e3eb21ca23abd7
JKumamoto <jeff.kmt@gmail.com>
2015-08-13 20:07:23 -0300

    Game Over ajustado, fragmentconfig ajustado pro landscape

--- ShooterGenerico/src/br/edu/ufabc/android/shootergenerico/GameOver.java
+++ ShooterGenerico/src/br/edu/ufabc/android/shootergenerico/GameOver.java
@@ -2,9 +2,12 @@ package br.edu.ufabc.android.shootergenerico;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.TextView;
 import br.edu.ufabc.android.menu.MenuActivity;
 
@@ -13,6 +16,10 @@ public class GameOver extends Activity{
 	protected void onCreate(Bundle savedInstanceState){
 		Log.d("oncreate", "GameOver");
 		super.onCreate(savedInstanceState);
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		
+		setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
 		this.setContentView(R.layout.gameover);
 		TextView te=(TextView) findViewById(R.id.textView2);
 		te.setText("Pontuacao: "+Parametros.score);
